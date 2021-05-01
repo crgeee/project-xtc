62645b30bbeca976a61ea11158754c23b2167be2
Christopher Gonzalez <crg167@gmail.com>
2021-05-01 12:14:46 -0700

    fix: moar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,24 @@
+const tailwindConfig = require("./tailwind.config.js");
+
 module.exports = {
   siteMetadata: {
     title: "project-xtc",
+    description: `Gatsby starter styled with Tailwind`,
+    author: `project-xtc`,
   },
   plugins: [
-    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`tailwindcss`)(tailwindConfig),
+          require(`autoprefixer`),
+          ...(process.env.NODE_ENV === `production`
+            ? [require(`cssnano`)]
+            : []),
+        ],
+      },
+    },
     "gatsby-plugin-netlify",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
